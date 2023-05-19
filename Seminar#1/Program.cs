686be889b6259e89c154a12c4b484b62a5a8355c
unknown <  >
2023-05-19 10:37:56 +0300

    Feat add new textwork

---
+++ Seminar#1/Program.cs
@@ -0,0 +1,48 @@
+﻿// '32' 32
+// string int (double, float)
+
+// '32' + '78' = 3278
+//  32  +  78  = 110
+// Console.Write()          - Однострочная
+// Console.WriteLine()      - Переходить на новую строку
+
+// Console.Write("Good morning! ");
+// Console.WriteLine("input your number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{number}");
+
+// Задача№1
+//  Напишите программу, которая на вход принимает число и
+//  выводить его квадрат.
+
+// Console.WriteLine("input number: ");
+// int nUm = Convert.ToInt32(Console.ReadLine());
+// int temp = nUm * nUm;
+
+// Console.WriteLine($"{nUm} -> " + (temp));
+
+// Задача№2
+// Напишите программу, которая принимает два числа и
+// проверяет, что первое число является квадратом второго.
+
+// 1. Запросить два знач.
+// 2. Возвести 2ое число в квадрат
+// 3. Проверить на равенство 1ому числу
+
+// Console.WriteLine("input 1st number: ");
+// int num_Ber1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("input 2st number: ");
+// int num_Ber2 = Convert.ToInt32(Console.ReadLine());
+
+// int temp = num_Ber2 * num_Ber2;
+
+// if(num_Ber1 == temp)
+// {
+//     Console.WriteLine($"{num_Ber2} * {num_Ber2} = {num_Ber1} -> yes");
+// }
+// else
+// {
+//     Console.WriteLine($"{num_Ber2} * {num_Ber2} = {num_Ber1} -> no ");
+// }
+
