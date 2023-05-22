f7680b0117e73119a55b73012e343ffdb00c8f88
unknown <  >
2023-05-22 21:35:49 +0300

    Feat add 5 textwork

--- Seminar#1/Program.cs
+++ Seminar#1/Program.cs
@@ -44,5 +44,56 @@
 // else
 // {
 //     Console.WriteLine($"{num_Ber2} * {num_Ber2} = {num_Ber1} -> no ");
-// }
+
+// Задача№3
+// Напишите программу, которая на вход принимает одно число N,
+// а на выходе показывает все целые числа от -N до N.
+/*
+Console.WriteLine("input 1st number: ");
+int number_N = Convert.ToInt32(Console.ReadLine());
+
+int current_i = number_N * (-1);
+
+while(current_i <= number_N)
+{
+    Console.Write(current_i + " ");
+    current_i++;
+}
+*/
+
+// Задача№4
+// Напишите программу, которая принимает на вход трехзначное число и
+// на выходе показывает последнюю цифру этого числа.
+
+// 1. Ввод числа
+
+// Console.WriteLine("input number: ");
+// int num_N = Convert.ToInt32(Console.ReadLine());
+
+// 2. Проверить на трехзначность  (N > 99 || N < 1000)
+// 3. Выявить последнюю цифру: n % 10
+    // 456 % 10  -> 6
+    // 456 % 100 -> 56
+    // 456 / 10  -> 45
+    // 456 / 100 -> 4
+
+/*
+Console.WriteLine("input number: ");
+int num_N = Convert.ToInt32(Console.ReadLine());
+
+if(num_N > 99 && num_N < 1000)
+{
+    // int num_dev = num_N % 10;
+    // Console.WriteLine($"{num_N} -> {num_dev}");
+    Console.WriteLine($"{num_N} -> {num_N % 10}");
+}
+else
+{
+    Console.WriteLine("Your number is not 3-digits");
+}
+*/
+
+
+
+
 
