2dbbd8f4eda25d9635d384d5d3340e0602bd5cdd
unknown <  >
2023-06-08 12:51:36 +0300

    Feat add new seminar6 cher

---
+++ работа/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Explanation(объяснение):
+Simple application to show current work activity of 
+all employers in organization.
+(Простое приложение для отображения текущей рабочей активности всех 
+работодателей в организации)
+
+Client - c/c++
+silent launches on user logon and work in background
+Communicates with server at any protocol.
+(автоматический запуск при входе пользователя в систему и работа в фоновом режиме
+Связь с сервером по любому протоколу)
+
+Server - desktop or web interface - any language
+List all connected clients - domain/machine/ip/user
+Show client's last active time
+Ability to get screenshot from client's desktop.
+(Сервер - рабочий стол или веб-интерфейс - любой язык
+Список всех подключенных клиентов - домен/компьютер/ip/пользователь
+Показать время последней активности клиента
+Возможность сделать скриншот с рабочего стола клиентa
+*/
+// int data = 
+Console.WriteLine("Welcome to the server: desktop");
+
+
+
+
+
+
+
+
